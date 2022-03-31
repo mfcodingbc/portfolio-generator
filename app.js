966ae6f493a520f7bbfedd950f1832cb2da36bcd
Mitchell Frey <mfcodingbc@gmail.com>
2022-03-31 17:35:45 -0400

    Create HTML file from app.js page-template file holds HTML creation function, error callback function

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-// console log of array comprised of the strings typed in console
+// requiring the file system module (from node.js files) which allows for file creation
+const fs = require('fs');
+// requiring the page-template.js module (from local files) which creates the HTML page
+const generatePage = require('./src/page-template');
+
+// array comprised of the strings typed in console (the user command-line arguments)
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-// console log of 2 different ways to print the individual objects of an array that were typed in the console
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// taking the array and assigning variables to the strings at each index
+const [name, github] = profileDataArgs;
 
-    console.log('====================');
+// // console log of 2 different ways to print the individual objects of an array that were typed in the console
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     console.log('====================');
 
-printProfileData(profileDataArgs);
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
+// printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
