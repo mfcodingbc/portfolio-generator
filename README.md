5f05b980ae83de015edb99b6f7ab73ce5112b717
Mitchell Frey <mfcodingbc@gmail.com>
2022-04-01 01:18:03 -0400

    project complete, README updated refactored app.js with promises, generate-site.js created, styles added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# portfolio-generator
+# Portfolio-Generator
+
+## Description
+Create a portfolio page of your GitHub projects following instructions all in the command-line.
+
+## Installation
+Clone the code to your local machine and run the app.js file through Node.js in the terminal.
+
+## Usage
+After answering all questions, you will have an index.html file created in the 'dist' folder. Open this in a browser to look over the completed Portfolio Page.
+
+## Credits
+
+Created following the MSU Coding Bootcamp module on Node.js.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// requiring the file system module (from node.js files) which allows for file creation
-const fs = require('fs');
-
 const inquirer = require('inquirer');
 
 // requiring the page-template.js module (from local files) which creates the HTML page
 const generatePage = require('./src/page-template');
+
+// requiring the generate-site.js module (from local files) which creates the files for the site and copies the style sheet
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -126,79 +126,49 @@ const promptProject = portfolioData => {
             default: false
         }
     ])
-    .then(projectData => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-            return promptProject(portfolioData);
-        } else {
-            return portfolioData;
-        }
-    });
-};
-
-// MockData Section, remove once finished with development
-const mockData = {
-    name: 'Lernantino',
-  github: 'lernantino',
-  confirmAbout: true,
-  about:
-    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-  projects: [
-    {
-      name: 'Run Buddy',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['HTML', 'CSS'],
-      link: 'https://github.com/lernantino/run-buddy',
-      feature: true,
-      confirmAddProject: true
-    },
-    {
-      name: 'Taskinator',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['JavaScript', 'HTML', 'CSS'],
-      link: 'https://github.com/lernantino/taskinator',
-      feature: true,
-      confirmAddProject: true
-    },
-    {
-      name: 'Taskmaster Pro',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-      link: 'https://github.com/lernantino/taskmaster-pro',
-      feature: false,
-      confirmAddProject: true
-    },
-    {
-      name: 'Robot Gladiators',
-      description:
-        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-      languages: ['JavaScript'],
-      link: 'https://github.com/lernantino/robot-gladiators',
-      feature: false,
-      confirmAddProject: false
-    }
-  ]
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        });
 };
 
 const pageHTML = generatePage(mockData);
 
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw new Error(err);
+fs.writeFile('./dist/index.html', pageHTML, err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Page created! Check out index.html in dist to see it!')
 
-    console.log('Page created! Check out index.html in this directory to see it!')
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Style sheet copied successfully!');
+    });
 });
 
-// promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//         const pageHTML = generatePage(portfolioData);
-
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //     if (err) throw new Error(err);
-
-        //     console.log('Page created! Check out index.html in this directory to see it!')
-        // });
-    // });
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+        })
+    .then(pageHTML => {
+        return fs.writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
