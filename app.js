c9c4fb0c422c836376b27964ff0dace0a295fb15
Mitchell Frey <mfcodingbc@gmail.com>
2022-04-01 00:00:01 -0400

    incorporated user input data into HTML creation function in page-template creates more HTML elements and uses user input

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+// requiring the file system module (from node.js files) which allows for file creation
+const fs = require('fs');
+
 const inquirer = require('inquirer');
+
+// requiring the page-template.js module (from local files) which creates the HTML page
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -130,20 +136,69 @@ const promptProject = portfolioData => {
     });
 };
 
-promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        console.log(portfolioData);
-    });
-// // requiring the file system module (from node.js files) which allows for file creation
-// const fs = require('fs');
-// // requiring the page-template.js module (from local files) which creates the HTML page
-// const generatePage = require('./src/page-template');
+// MockData Section, remove once finished with development
+const mockData = {
+    name: 'Lernantino',
+  github: 'lernantino',
+  confirmAbout: true,
+  about:
+    'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
+  projects: [
+    {
+      name: 'Run Buddy',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['HTML', 'CSS'],
+      link: 'https://github.com/lernantino/run-buddy',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskinator',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'HTML', 'CSS'],
+      link: 'https://github.com/lernantino/taskinator',
+      feature: true,
+      confirmAddProject: true
+    },
+    {
+      name: 'Taskmaster Pro',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
+      languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
+      link: 'https://github.com/lernantino/taskmaster-pro',
+      feature: false,
+      confirmAddProject: true
+    },
+    {
+      name: 'Robot Gladiators',
+      description:
+        'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
+      languages: ['JavaScript'],
+      link: 'https://github.com/lernantino/robot-gladiators',
+      feature: false,
+      confirmAddProject: false
+    }
+  ]
+};
 
-// const pageHTML = generatePage(name, github);
+const pageHTML = generatePage(mockData);
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
 
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+    console.log('Page created! Check out index.html in this directory to see it!')
+});
+
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         const pageHTML = generatePage(portfolioData);
+
+        // fs.writeFile('./index.html', pageHTML, err => {
+        //     if (err) throw new Error(err);
+
+        //     console.log('Page created! Check out index.html in this directory to see it!')
+        // });
+    // });
